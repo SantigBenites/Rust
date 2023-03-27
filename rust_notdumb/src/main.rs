79ce137e65de58a4db18d928c8475f5d210c6502
Santiago Benites <santi.mbenites@gmail.com>
2023-03-27 17:03:30 +0100

    Made it so that server works with requests, and can now have multiple HTML and css

--- rust_notdumb/src/main.rs
+++ rust_notdumb/src/main.rs
@@ -3,7 +3,7 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    env
+    env,
 };
 
 use regex::Regex;
@@ -30,26 +30,40 @@ fn handle_connection(mut stream: TcpStream) {
 
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
+    println!("Request line is {request_line}");
 
     
-    let re = Regex::new(r"GET / HTTP/1.1").unwrap();
+    let re = Regex::new(r"GET (.*) HTTP/1.1").unwrap();
 
     let (status_line, filename) = if re.is_match(&request_line) {
-
         let fileregex = Regex::new(r"/(.*) ").unwrap();
-        let filename : String= fileregex.find_iter(&request_line).map(|mat| mat.as_str()).collect();
-        let finalname:&'static str = str::new(filename.as_str());
-        ("HTTP/1.1 200 OK", finalname)
+        let temp = fileregex.find(&request_line).map(|mat| mat.as_str());
+        
+        match temp {
+            Some(str) => ("HTTP/1.1 200 OK", str),
+            None =>  ("HTTP/1.1 404 NOT FOUND", "404.html")
+        }
+        
 
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
+    // Combining filename and "." so that rust knows file location
+    let temp = ".".to_owned() + filename;
+    // Removing last space from temp, it messes with filenames
+    let name = &&temp[0..temp.len() - 1];
+
+    let content = match filename{
+        "/ " => fs::read_to_string("./html/hello.html").unwrap(),
+        "404.html" => fs::read_to_string("./html/404.html").unwrap(),
+        _ => fs::read_to_string(name).unwrap()
+    };
+
+    let length = content.len();
 
     let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
